108594bafa334bd433d26978a195409caaffc71f
marianozzz <mariano.zanier@gmail.com>
2021-06-17 19:26:00 -0300

    navbar

--- login.php
+++ login.php
@@ -1,46 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-<?php include ('partials/head.php'); ?>
-
-    <title>Inicio de Sesion</title>
-</head>
-<body>
-<div class="container">
-
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card mt-4">
-                <div class="card-header bg-success">
-                    <h5>Iniciar sesion</h5>
-                </div>
-                    <div class="card-body">
-               <!-- Inicio del fomulario -->       
-                    <form action="LoginController.php" method="post">
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" class="form-control">
-                            </div>
-
-                            <input type="submit" value="Iniciar Sesion" class="btn btn-success bt-sm mt-2">
-                    </form>
-                <!-- Fin del fomulario -->  
-                    </div>
-                    <div class="card-footer bg-dark">
-                        <h5>pie pagina </h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-    
-    </div>
-    <?php include('partials/footer.php'); ?>
-=======
   <?php
   include('partials/head.php');
   ?>
@@ -75,6 +35,5 @@
             </div>  <!-- Cierra el col -->
         </div><!-- Cierra el row -->
     </div><!-- Cierra el container -->
->>>>>>> 36439649fc3af72864707245382de318f4f93985
 </body>
 </html>
